bd6a88281c302d751916ef234f57c6d5214e9cc1
denz-denz <chua.alden@gmail.com>
2025-08-20 17:20:09 +0800

    Added ability to mark task as done

--- src/main/java/Denz.java
+++ src/main/java/Denz.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Denz {
+    private static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo = " ____  _____ _   _ _____ \n"
@@ -9,7 +17,7 @@ public class Denz {
                 + "| |_| | |___| |\\  |/ /_| \n"
                 + "|____/|_____|_| \\_|____|\n";
         String newLine = "--------------------------------------------------";
-        ArrayList<String> userInputs = new ArrayList<>();
+        ArrayList<Task> userInputs = new ArrayList<>();
         System.out.println("Hello from\n" + logo);
         System.out.println(newLine);
         System.out.println("Hello, I'm Denz");
@@ -17,7 +25,48 @@ public class Denz {
         System.out.println(newLine);
         while (sc.hasNextLine()){
             String line = sc.nextLine().trim();
-            if (line.equalsIgnoreCase("bye")){
+            String[] parts = line.trim().split("\\s+");
+            if (parts[0].equalsIgnoreCase("mark") && parts.length == 2){
+                if (!isInteger(parts[1])){
+                    System.out.println("added: " + newLine);
+                    System.out.println("    " + line);
+                    System.out.println(newLine);
+                    userInputs.add(new Task(line));
+                }
+                else{
+                    try {
+                        int index = Integer.parseInt(parts[1]);
+                        Task task = userInputs.get(index-1);
+                        task.mark();
+                        System.out.println("Yay! I have successfully marked this task as done:");
+                        System.out.println(task);
+                    }
+                    catch (NumberFormatException e){
+                        System.out.println("invalid task index to mark");
+                    }
+                }
+            }
+            else if (parts[0].equalsIgnoreCase("unmark") && parts.length == 2){
+                if (!isInteger(parts[1])){
+                    System.out.println("added: " + newLine);
+                    System.out.println("    " + line);
+                    System.out.println(newLine);
+                    userInputs.add(new Task(line));
+                }
+                else {
+                    try {
+                        int index = Integer.parseInt(parts[1]);
+                        Task task = userInputs.get(index-1);
+                        task.unmark();
+                        System.out.println("Aw man! Okay I have helped unmark this task. Hang in there:");
+                        System.out.println(task);
+                    }
+                    catch (NumberFormatException e){
+                        System.out.println("invalid task index to mark");
+                    }
+                }
+            }
+            else if (line.equalsIgnoreCase("bye")){
                 System.out.println(newLine);
                 System.out.println("    Finally, time to take a break!");
                 System.out.println(newLine);
@@ -25,17 +74,19 @@ public class Denz {
             }
             else if (line.equalsIgnoreCase("list")){
                 for (int i = 0; i < userInputs.size(); i ++){
-                    System.out.println(i + ": " + userInputs.get(i));
+                    System.out.println(i+1 + ": " + userInputs.get(i));
                 }
             }
             else if (line.equals("")){
                 continue;
             }
+
+
             else{
                 System.out.println("added: " + newLine);
                 System.out.println("    " + line);
                 System.out.println(newLine);
-                userInputs.add(line);
+                userInputs.add(new Task(line));
             }
 
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+    private boolean isDone;
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public boolean isDone(){
+        return this.isDone;
+    }
+    public void mark(){
+        this.isDone = true;
+    }
+    public void unmark(){
+        this.isDone = false;
+    }
+    @Override
+    public String toString(){
+        return isDone ? "[X] " + this.getDescription() : "[ ] " + this.getDescription();
+    }
 }
