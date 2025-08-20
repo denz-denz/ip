bbc45894b762fccc1c999ed896b0de4e913748a1
denz-denz <chua.alden@gmail.com>
2025-08-20 16:53:44 +0800

    added ability to store user text and display when requested

--- src/main/java/Denz.java
+++ src/main/java/Denz.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Denz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,22 +9,35 @@ public class Denz {
                 + "| |_| | |___| |\\  |/ /_| \n"
                 + "|____/|_____|_| \\_|____|\n";
         String newLine = "--------------------------------------------------";
+        ArrayList<String> userInputs = new ArrayList<>();
         System.out.println("Hello from\n" + logo);
         System.out.println(newLine);
         System.out.println("Hello, I'm Denz");
         System.out.println("What can I do for you?");
         System.out.println(newLine);
         while (sc.hasNextLine()){
-            String line = sc.nextLine();
-            if (line.equals("bye")){
+            String line = sc.nextLine().trim();
+            if (line.equalsIgnoreCase("bye")){
                 System.out.println(newLine);
                 System.out.println("    Finally, time to take a break!");
                 System.out.println(newLine);
                 System.exit(0);
             }
-            System.out.println(newLine);
-            System.out.println("    " + line);
-            System.out.println(newLine);
+            else if (line.equalsIgnoreCase("list")){
+                for (int i = 0; i < userInputs.size(); i ++){
+                    System.out.println(i + ": " + userInputs.get(i));
+                }
+            }
+            else if (line.equals("")){
+                continue;
+            }
+            else{
+                System.out.println("added: " + newLine);
+                System.out.println("    " + line);
+                System.out.println(newLine);
+                userInputs.add(line);
+            }
+
         }
         sc.close();
     }
