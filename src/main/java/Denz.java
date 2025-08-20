c21183be9b5422a09a02f7ae9b01047fcfedad30
denz-denz <chua.alden@gmail.com>
2025-08-20 15:10:34 +0800

    Improved Denz to echo back user input

--- src/main/java/Denz.java
+++ src/main/java/Denz.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
 public class Denz {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ____  _____ _   _ _____ \n"
                 + "|  _ \\| ____| \\ | |__  / \n"
                 + "| | | |  _| |  \\| | / / \n"
                 + "| |_| | |___| |\\  |/ /_| \n"
                 + "|____/|_____|_| \\_|____|\n";
+        String newLine = "--------------------------------------------------";
         System.out.println("Hello from\n" + logo);
-        System.out.println("--------------------------------------------------");
+        System.out.println(newLine);
         System.out.println("Hello, I'm Denz");
         System.out.println("What can I do for you?");
-        System.out.println("--------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-
+        System.out.println(newLine);
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+            if (line.equals("bye")){
+                System.out.println(newLine);
+                System.out.println("    Finally, time to take a break!");
+                System.out.println(newLine);
+                System.exit(0);
+            }
+            System.out.println(newLine);
+            System.out.println("    " + line);
+            System.out.println(newLine);
+        }
+        sc.close();
     }
 }
