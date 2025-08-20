940310ffc0c502db155c38dc23df5c5233bfd12c
denz-denz <chua.alden@gmail.com>
2025-08-20 14:16:37 +0800

    renamed chat bot to Denz and added greetings and goodbyes

---
+++ src/main/java/Denz.java
@@ -0,0 +1,16 @@
+public class Denz {
+    public static void main(String[] args) {
+        String logo = " ____  _____ _   _ _____ \n"
+                + "|  _ \\| ____| \\ | |__  / \n"
+                + "| | | |  _| |  \\| | / / \n"
+                + "| |_| | |___| |\\  |/ /_| \n"
+                + "|____/|_____|_| \\_|____|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Hello, I'm Denz");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+}
